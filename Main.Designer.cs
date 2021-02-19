bb657c3cbb61ad678e805157b6dc5dc8fac26555
Constantin <constantin@folkytools.com>
2021-02-19 10:15:54 +0100

    Filtering data

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -29,7 +29,6 @@ namespace InputsOutputs
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
@@ -38,7 +37,7 @@ namespace InputsOutputs
             this.tblPrincipal = new System.Windows.Forms.TableLayoutPanel();
             this.grdAll = new System.Windows.Forms.DataGridView();
             this.tblFooter = new System.Windows.Forms.TableLayoutPanel();
-            this.label9 = new System.Windows.Forms.Label();
+            this.lblTotalInfo = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.label8 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
@@ -54,25 +53,11 @@ namespace InputsOutputs
             this.btnAddCost = new System.Windows.Forms.Button();
             this.btnAddSale = new System.Windows.Forms.Button();
             this.imgLogo = new System.Windows.Forms.PictureBox();
-            this.inputsOutputsDataSet = new InputsOutputs.InputsOutputsDataSet();
-            this.dataBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.dataTableAdapter = new InputsOutputs.InputsOutputsDataSetTableAdapters.DataTableAdapter();
-            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tblPrincipal.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.grdAll)).BeginInit();
             this.tblFooter.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.inputsOutputsDataSet)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // lblTitle
@@ -115,7 +100,6 @@ namespace InputsOutputs
             this.grdAll.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.grdAll.AutoGenerateColumns = false;
             this.grdAll.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.grdAll.BackgroundColor = System.Drawing.SystemColors.Control;
             this.grdAll.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
@@ -129,17 +113,6 @@ namespace InputsOutputs
             this.grdAll.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.grdAll.ColumnHeadersHeight = 35;
             this.grdAll.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-            this.grdAll.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.dataGridViewTextBoxColumn1,
-            this.dataGridViewTextBoxColumn2,
-            this.dataGridViewTextBoxColumn3,
-            this.dataGridViewTextBoxColumn4,
-            this.dataGridViewTextBoxColumn5,
-            this.dataGridViewTextBoxColumn6,
-            this.dataGridViewTextBoxColumn7,
-            this.dataGridViewTextBoxColumn8,
-            this.dataGridViewTextBoxColumn9});
-            this.grdAll.DataSource = this.dataBindingSource;
             this.grdAll.Location = new System.Drawing.Point(3, 43);
             this.grdAll.MultiSelect = false;
             this.grdAll.Name = "grdAll";
@@ -168,7 +141,7 @@ namespace InputsOutputs
             this.tblFooter.ColumnCount = 2;
             this.tblFooter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45F));
             this.tblFooter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 55F));
-            this.tblFooter.Controls.Add(this.label9, 0, 0);
+            this.tblFooter.Controls.Add(this.lblTotalInfo, 0, 0);
             this.tblFooter.Controls.Add(this.tableLayoutPanel1, 1, 0);
             this.tblFooter.Location = new System.Drawing.Point(3, 391);
             this.tblFooter.Name = "tblFooter";
@@ -177,18 +150,18 @@ namespace InputsOutputs
             this.tblFooter.Size = new System.Drawing.Size(1185, 86);
             this.tblFooter.TabIndex = 1;
             // 
-            // label9
+            // lblTotalInfo
             // 
-            this.label9.AutoSize = true;
-            this.label9.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
-            this.label9.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label9.Location = new System.Drawing.Point(3, 0);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(527, 86);
-            this.label9.TabIndex = 1;
-            this.label9.Text = "TOTAL [01.01.2021 - 01.03.2021]             ";
-            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblTotalInfo.AutoSize = true;
+            this.lblTotalInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTotalInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.lblTotalInfo.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblTotalInfo.Location = new System.Drawing.Point(3, 0);
+            this.lblTotalInfo.Name = "lblTotalInfo";
+            this.lblTotalInfo.Size = new System.Drawing.Size(527, 86);
+            this.lblTotalInfo.TabIndex = 1;
+            this.lblTotalInfo.Text = "TOTAL";
+            this.lblTotalInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // tableLayoutPanel1
             // 
@@ -393,85 +366,6 @@ namespace InputsOutputs
             this.imgLogo.TabIndex = 0;
             this.imgLogo.TabStop = false;
             // 
-            // inputsOutputsDataSet
-            // 
-            this.inputsOutputsDataSet.DataSetName = "InputsOutputsDataSet";
-            this.inputsOutputsDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // dataBindingSource
-            // 
-            this.dataBindingSource.DataMember = "Data";
-            this.dataBindingSource.DataSource = this.inputsOutputsDataSet;
-            // 
-            // dataTableAdapter
-            // 
-            this.dataTableAdapter.ClearBeforeFill = true;
-            // 
-            // dataGridViewTextBoxColumn1
-            // 
-            this.dataGridViewTextBoxColumn1.DataPropertyName = "Id";
-            this.dataGridViewTextBoxColumn1.HeaderText = "Id";
-            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
-            this.dataGridViewTextBoxColumn1.ReadOnly = true;
-            this.dataGridViewTextBoxColumn1.Visible = false;
-            // 
-            // dataGridViewTextBoxColumn2
-            // 
-            this.dataGridViewTextBoxColumn2.DataPropertyName = "SellerName";
-            this.dataGridViewTextBoxColumn2.HeaderText = "Vanzator";
-            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
-            this.dataGridViewTextBoxColumn2.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn3
-            // 
-            this.dataGridViewTextBoxColumn3.DataPropertyName = "Date";
-            this.dataGridViewTextBoxColumn3.HeaderText = "Data";
-            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
-            this.dataGridViewTextBoxColumn3.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn4
-            // 
-            this.dataGridViewTextBoxColumn4.DataPropertyName = "Name";
-            this.dataGridViewTextBoxColumn4.HeaderText = "Denumire Piesa";
-            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
-            this.dataGridViewTextBoxColumn4.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn5
-            // 
-            this.dataGridViewTextBoxColumn5.DataPropertyName = "PriceRonCash";
-            this.dataGridViewTextBoxColumn5.HeaderText = "Pret RON (Cash)";
-            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
-            this.dataGridViewTextBoxColumn5.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn6
-            // 
-            this.dataGridViewTextBoxColumn6.DataPropertyName = "PriceRonCashRegister";
-            this.dataGridViewTextBoxColumn6.HeaderText = "Pret RON (C.M.)";
-            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
-            this.dataGridViewTextBoxColumn6.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn7
-            // 
-            this.dataGridViewTextBoxColumn7.DataPropertyName = "PriceEuro";
-            this.dataGridViewTextBoxColumn7.HeaderText = "Pret EURO";
-            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
-            this.dataGridViewTextBoxColumn7.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn8
-            // 
-            this.dataGridViewTextBoxColumn8.DataPropertyName = "PricePounds";
-            this.dataGridViewTextBoxColumn8.HeaderText = "Pret LIRE";
-            this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
-            this.dataGridViewTextBoxColumn8.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn9
-            // 
-            this.dataGridViewTextBoxColumn9.DataPropertyName = "Type";
-            this.dataGridViewTextBoxColumn9.HeaderText = "Type";
-            this.dataGridViewTextBoxColumn9.Name = "dataGridViewTextBoxColumn9";
-            this.dataGridViewTextBoxColumn9.ReadOnly = true;
-            this.dataGridViewTextBoxColumn9.Visible = false;
-            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -497,8 +391,6 @@ namespace InputsOutputs
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.imgLogo)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.inputsOutputsDataSet)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -523,30 +415,9 @@ namespace InputsOutputs
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblTotalInfo;
         private System.Windows.Forms.Button btnFilter;
         private System.Windows.Forms.Label lblFiltersInfo;
-        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn sellerNameDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dateDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn priceRonCashDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn priceRonCashRegisterDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn priceEuroDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn pricePoundsDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn typeDataGridViewTextBoxColumn;
-        private InputsOutputsDataSet inputsOutputsDataSet;
-        private System.Windows.Forms.BindingSource dataBindingSource;
-        private InputsOutputsDataSetTableAdapters.DataTableAdapter dataTableAdapter;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn9;
     }
 }
 
--- Main.cs
+++ Main.cs
@@ -1,6 +1,10 @@
 ﻿using InputsOutputs.Models.Database;
 using System;
+using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.Globalization;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace InputsOutputs
@@ -11,6 +15,9 @@ namespace InputsOutputs
         public static DateTime dateFromFilter = DateTime.Now.Date;
         public static DateTime dateToFilter = DateTime.Now.Date;
 
+        private BindingSource bindingSource = new BindingSource();
+        private SqlDataAdapter dataAdapter = new SqlDataAdapter();
+
         public Main()
         {
             InitializeComponent();
@@ -18,33 +25,33 @@ namespace InputsOutputs
 
         private void Main_Load(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'inputsOutputsDataSet.Data' table. You can move, or remove it, as needed.
-            this.dataTableAdapter.Fill(this.inputsOutputsDataSet.Data);
-            // TODO: This line of code loads data into the 'inputsOutputsDataSet2.Data' table. You can move, or remove it, as needed.
-            this.dataTableAdapter.Fill(this.inputsOutputsDataSet.Data);
-            dataBindingSource.Filter = string.Format("Date >= '{0:yyyy-MM-dd}' AND Date <= '{0:yyyy-MM-dd}'", dateFromFilter);
-            dataTableAdapter.Fill(inputsOutputsDataSet.Data);
+            grdAll.DataSource = bindingSource;
+            RefreshData();
+
             lblFiltersInfo.Text = $"FILTROS APLICADOS -> Data: [{dateFromFilter.ToShortDateString()}]";
             lblFiltersInfo.Visible = true;
+            lblTotalInfo.Text += $"TOTAL{Environment.NewLine}Data: [{dateFromFilter.ToShortDateString()}]";
+
+            FormatGridColums();
         }
 
         private void grdAll_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
         {
-            if (inputsOutputsDataSet.Data[e.RowIndex].Type == (int)DataType.Sale)
+            if ((int)grdAll.Rows[e.RowIndex].Cells[8].Value == (int)DataType.Sale)
             {
                 e.CellStyle.BackColor = Color.LightGreen;
                 e.CellStyle.ForeColor = Color.Black;
                 e.CellStyle.SelectionBackColor = Color.LightGreen;
                 e.CellStyle.SelectionForeColor = Color.Black;
             }
-            else if (inputsOutputsDataSet.Data[e.RowIndex].Type == (int)DataType.Cost)
+            else if ((int)grdAll.Rows[e.RowIndex].Cells[8].Value == (int)DataType.Cost)
             {
                 e.CellStyle.BackColor = Color.PaleVioletRed;
                 e.CellStyle.ForeColor = Color.White;
                 e.CellStyle.SelectionBackColor = Color.PaleVioletRed;
                 e.CellStyle.SelectionForeColor = Color.White;
             }
-            else if (inputsOutputsDataSet.Data[e.RowIndex].Type == (int)DataType.Return)
+            else if ((int)grdAll.Rows[e.RowIndex].Cells[8].Value == (int)DataType.Return)
             {
                 e.CellStyle.BackColor = Color.Khaki;
                 e.CellStyle.ForeColor = Color.Black;
@@ -60,17 +67,65 @@ namespace InputsOutputs
             filterForm.ShowDialog();
         }
 
+        private void RefreshData()
+        {
+            dataAdapter = new SqlDataAdapter(
+                $@"EXEC [dbo].[GetAll]
+                    @searchTerm = {(!string.IsNullOrEmpty(partNameFilter) ? $"N'{GetSearchTerm(partNameFilter)}'" : "NULL")},
+                    @from = N'{string.Format("{0:yyyy-MM-dd}", dateFromFilter)}',
+                    @to = N'{string.Format("{0:yyyy-MM-dd}", dateToFilter)}'",
+                Properties.Settings.Default.DefaultConnection);
+
+            var table = new DataTable { Locale = CultureInfo.InvariantCulture };
+            dataAdapter.Fill(table);
+            bindingSource.DataSource = table;
+        }
+
+        private void FormatGridColums()
+        {
+            grdAll.Columns[0].Visible = false;
+            grdAll.Columns[1].HeaderText = "Vanzator";
+            grdAll.Columns[2].HeaderText = "Data";
+            grdAll.Columns[3].HeaderText = "Denumire Piesa";
+            grdAll.Columns[4].HeaderText = "Pret RON (Cash)";
+            grdAll.Columns[5].HeaderText = "Pret RON (C.M.)";
+            grdAll.Columns[6].HeaderText = "Pret EURO";
+            grdAll.Columns[7].HeaderText = "Pret LIRE";
+            grdAll.Columns[8].Visible = false;
+        }
+
         private void OnFilterApplied(object sender, EventArgs e)
         {
-            lblFiltersInfo.Text = $"FILTROS APLICADOS -> ";
-            if (!string.IsNullOrEmpty(partNameFilter))
-                lblFiltersInfo.Text += $"Denumire: [{partNameFilter}], ";
-            if (dateFromFilter == dateToFilter)
-                lblFiltersInfo.Text += $"Data: [{dateFromFilter.ToShortDateString()}]";
-            else
-                lblFiltersInfo.Text += $"Perioada: [{dateFromFilter.ToShortDateString()} - {dateToFilter.ToShortDateString()}]";
+            lblFiltersInfo.Text = "FILTROS APLICADOS -> ";
+            lblTotalInfo.Text = "TOTAL";
 
-            dataBindingSource.Filter = string.Format("Date >= '{0:yyyy-MM-dd}' AND Date <= '{1:yyyy-MM-dd}'", dateFromFilter, dateToFilter);
+            if (!string.IsNullOrEmpty(partNameFilter))
+            {
+                lblFiltersInfo.Text += $"Denumire: [{partNameFilter}], ";
+                lblTotalInfo.Text += $"{Environment.NewLine}Termen de cautare: [{partNameFilter}]";
+            }
+            if (dateFromFilter == dateToFilter)
+            {
+                lblFiltersInfo.Text += $"Data: [{dateFromFilter.ToShortDateString()}]";
+                lblTotalInfo.Text += $"{Environment.NewLine}Data: [{dateFromFilter.ToShortDateString()}]";
+            }
+            else
+            {
+                lblFiltersInfo.Text += $"Perioada: [{dateFromFilter.ToShortDateString()} - {dateToFilter.ToShortDateString()}]";
+                lblTotalInfo.Text += $"{Environment.NewLine}Perioada: [{dateFromFilter.ToShortDateString()} - {dateToFilter.ToShortDateString()}]";
+            }
+
+            RefreshData();
+        }
+
+        public string GetSearchTerm(string partNameSearchTerm)
+        {
+            partNameSearchTerm = partNameSearchTerm.Replace("&", "&amp;").Replace("<", "&lt;").Replace(">", "&gt;");
+            return string.Format(
+                $"(CONTAINS (d.{nameof(DataModel.Name)}, ''{{0}}''))",
+                string.Join(" AND ", partNameSearchTerm.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                                                        .Select(x => x = string.Format("\"{0}*\"", x)))
+            );
         }
     }
 }
