897a0e27b2b0c55fa12eb524f88c19a6f0aaea59
Constantin <ccuciurean@outlook.com>
2023-08-16 07:56:48 +0200

    Republish

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace InputsOutputs.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,9 +26,9 @@ namespace InputsOutputs.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Server=.\\;Initial Catalog=InputsOutputs;User Id=inputs-outputs;Password=2ZavPeFWE" +
-            "85DJXHA;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;Application" +
-            "Intent=ReadWrite;MultiSubnetFailover=False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Server=ASPIRE-V3\\SQLEXPRESS;Initial Catalog=InputsOutputs;User Id=inputs-outputs;" +
+            "Password=2ZavPeFWE85DJXHA;Connect Timeout=30;Encrypt=False;TrustServerCertificat" +
+            "e=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")]
         public string DefaultConnection {
             get {
                 return ((string)(this["DefaultConnection"]));
